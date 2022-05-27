f3c03d4e130d03d9ba12f59ecdcc62c6fd9f2f77
Michael Klein <Michaelklein1997@gmail.com>
2022-05-27 10:11:59 -0500

    initial commit

---
+++ train-reservation/src/main/java/edu/smu/trainreservation/TrainReservationApplication.java
@@ -0,0 +1,13 @@
+package edu.smu.trainreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainReservationApplication.class, args);
+	}
+
+}
---
+++ train-reservation/src/test/java/edu/smu/trainreservation/TrainReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.smu.trainreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
